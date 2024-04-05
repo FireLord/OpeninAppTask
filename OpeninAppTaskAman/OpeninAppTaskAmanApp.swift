bb89d03ed48ab3b584eadf37abf251a3a064a146
FireLord <firelord.xda@gmail.com>
2024-04-05 17:30:46 +0530

    Initial Commit

---
+++ OpeninAppTaskAman/OpeninAppTaskAmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninAppTaskAmanApp.swift
+//  OpeninAppTaskAman
+//
+//  Created by Aman Kumar on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OpeninAppTaskAmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
